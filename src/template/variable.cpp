dd681c1614273ae0a9e82316d0a4769efac84120
PhilipHsu609 <mpyhacct118113@gmail.com>
2024-11-05 17:55:35 +0800

    Add more examples for template variable

--- src/template/variable.cpp
+++ src/template/variable.cpp
@@ -1,6 +1,7 @@
 /*
     Template variables
 */
+#include <array>       // std::array
 #include <cstddef>     // size_t
 #include <cstring>     // std::memcpy
 #include <memory>      // std::uninitialized_fill_n
@@ -9,6 +10,9 @@
 // can be pi<float>, pi<double>, pi<long double>
 template <typename T>
 constexpr T pi = T(3.1415926535897932385L);
+
+template <int N>
+std::array<int, N> arr{};
 
 template <typename T>
 T circular_area(T r) {
@@ -23,4 +27,14 @@ void init(T *p, size_t N) {
     } else {
         std::uninitialized_fill_n(p, N, T{});
     }
+}
+
+int main() {
+    arr<10>[0] = 1;
+
+    for (int i = 0; i < 10; i++) {
+        arr<10>[i] = i;
+    }
+
+    arr<13>[1] = 2;
 }
