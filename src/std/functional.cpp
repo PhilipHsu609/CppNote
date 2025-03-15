7796141887c40d50d4172ddf387e7e846946dace
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-03-15 15:46:43 +0800

    Add example for `std::invoke`

--- src/std/functional.cpp
+++ src/std/functional.cpp
@@ -1,6 +1,7 @@
-#include <functional> // function, bind, placeholders
-
 #include <fmt/core.h>
+
+#include <functional> // function, bind, placeholders
+#include <utility>
 
 struct A {
     void a(int n) { fmt::print("A::a({})\n", n); }
@@ -9,6 +10,20 @@ struct A {
 void foo(int a, int b, int c, int d, int e) {
     fmt::print("a: {}, b: {}, c: {}, d: {}, e: {}\n", a, b, c, d, e);
 }
+
+// A wrapper for std::invoke
+template <typename Callable, typename... Args>
+decltype(auto) call(Callable &&op, Args &&...args) {
+    return std::invoke(std::forward<Callable>(op), std::forward<Args>(args)...);
+}
+
+int add(int a, int b) { return a + b; }
+
+struct Multiply {
+    int operator()(int a, int b) const { return a * b; }
+};
+
+auto subtract = [](int a, int b) { return a - b; };
 
 int main() {
     // function object is a general-purpose polymorphic function wrapper
@@ -35,4 +50,10 @@ int main() {
     // or use std::mem_fn, a wrapper for member function
     auto a_wrap2 = std::mem_fn(&A::a);
     a_wrap2(a, 2);
+
+    // call function with std::invoke
+    call(add, 1, 2);
+    call(Multiply{}, 3, 4);
+    call(subtract, 5, 6);
+    call([](int a, int b) { return a / b; }, 7, 8);
 }
